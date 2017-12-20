8fb22fc33a3e852ae7fccf13597d37b664c25616
denniskorir <dkdenniskorir@gmail.com>
2017-12-20 19:13:36 +0300

    Relationship created

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -3,8 +3,14 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Review;
 
 class Product extends Model
 {
     //
+
+    Public function review(){
+
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Model;
+use App\Model\Product;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
     //
+    Public function product(){
+
+        return $this->belongsTo(Product::class);
+    }
 }
